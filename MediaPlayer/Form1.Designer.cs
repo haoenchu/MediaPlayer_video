0240b4598cdb202d4ccb7aedc5b20f58ec68abca
haoenchu <haoenchu@gmail.com>
2026-05-10 10:04:37 +0800

    加入專案檔案。

---
+++ MediaPlayer/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace MediaPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.wmpVideo = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.lstPlaylist = new System.Windows.Forms.ListBox();
+            this.picStop = new System.Windows.Forms.PictureBox();
+            this.picPause = new System.Windows.Forms.PictureBox();
+            this.picPlay = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picStop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPause)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.wmpVideo);
+            this.panel2.Location = new System.Drawing.Point(12, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1166, 639);
+            this.panel2.TabIndex = 2;
+            // 
+            // wmpVideo
+            // 
+            this.wmpVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wmpVideo.Enabled = true;
+            this.wmpVideo.Location = new System.Drawing.Point(0, 0);
+            this.wmpVideo.Name = "wmpVideo";
+            this.wmpVideo.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpVideo.OcxState")));
+            this.wmpVideo.Size = new System.Drawing.Size(1166, 639);
+            this.wmpVideo.TabIndex = 0;
+            this.wmpVideo.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.picStop);
+            this.panel1.Controls.Add(this.picPause);
+            this.panel1.Controls.Add(this.picPlay);
+            this.panel1.Controls.Add(this.btnBrowser);
+            this.panel1.Location = new System.Drawing.Point(12, 648);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel1.Size = new System.Drawing.Size(1166, 92);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.BackColor = System.Drawing.Color.Transparent;
+            this.btnBrowser.Cursor = System.Windows.Forms.Cursors.No;
+            this.btnBrowser.Font = new System.Drawing.Font("微軟正黑體", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBrowser.Location = new System.Drawing.Point(3, 3);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnBrowser.Size = new System.Drawing.Size(131, 86);
+            this.btnBrowser.TabIndex = 0;
+            this.btnBrowser.Text = "瀏覽";
+            this.btnBrowser.UseVisualStyleBackColor = false;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // lstPlaylist
+            // 
+            this.lstPlaylist.BackColor = System.Drawing.Color.PapayaWhip;
+            this.lstPlaylist.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lstPlaylist.FormattingEnabled = true;
+            this.lstPlaylist.HorizontalScrollbar = true;
+            this.lstPlaylist.ItemHeight = 24;
+            this.lstPlaylist.Location = new System.Drawing.Point(1201, 0);
+            this.lstPlaylist.Name = "lstPlaylist";
+            this.lstPlaylist.Size = new System.Drawing.Size(233, 752);
+            this.lstPlaylist.TabIndex = 4;
+            this.lstPlaylist.SelectedIndexChanged += new System.EventHandler(this.lstPlaylist_SelectedIndexChanged);
+            this.lstPlaylist.DoubleClick += new System.EventHandler(this.lstPlaylist_DoubleClick);
+            // 
+            // picStop
+            // 
+            this.picStop.Image = global::MediaPlayer.Properties.Resources.stop;
+            this.picStop.Location = new System.Drawing.Point(364, 7);
+            this.picStop.Name = "picStop";
+            this.picStop.Size = new System.Drawing.Size(71, 82);
+            this.picStop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picStop.TabIndex = 6;
+            this.picStop.TabStop = false;
+            this.picStop.Click += new System.EventHandler(this.picStop_Click);
+            // 
+            // picPause
+            // 
+            this.picPause.Image = global::MediaPlayer.Properties.Resources.pause;
+            this.picPause.Location = new System.Drawing.Point(267, 6);
+            this.picPause.Name = "picPause";
+            this.picPause.Size = new System.Drawing.Size(67, 83);
+            this.picPause.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picPause.TabIndex = 5;
+            this.picPause.TabStop = false;
+            this.picPause.Click += new System.EventHandler(this.picPause_Click);
+            // 
+            // picPlay
+            // 
+            this.picPlay.Image = global::MediaPlayer.Properties.Resources.play;
+            this.picPlay.Location = new System.Drawing.Point(171, 7);
+            this.picPlay.Name = "picPlay";
+            this.picPlay.Size = new System.Drawing.Size(71, 82);
+            this.picPlay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picPlay.TabIndex = 4;
+            this.picPlay.TabStop = false;
+            this.picPlay.Click += new System.EventHandler(this.picPlay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BurlyWood;
+            this.ClientSize = new System.Drawing.Size(1434, 752);
+            this.Controls.Add(this.lstPlaylist);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "多媒體播放器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picStop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPause)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private AxWMPLib.AxWindowsMediaPlayer wmpVideo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.ListBox lstPlaylist;
+        private System.Windows.Forms.PictureBox picPlay;
+        private System.Windows.Forms.PictureBox picPause;
+        private System.Windows.Forms.PictureBox picStop;
+    }
+}
+
---
+++ MediaPlayer/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AxWMPLib;
+
+namespace MediaPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true; // 開啟多選功能
+                                    // 設定過濾器，包含多種媒體格式
+            ofd.Filter = "影片檔案|*.mp4;*.avi;*.wmv|所有檔案|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // 關鍵：不要執行 lstPlaylist.Items.Clear()，這樣就能累加
+
+                foreach (string file in ofd.FileNames)
+                {
+                    // 選擇性功能：檢查清單中是否已經有這個檔案，避免重複加入
+                    if (!lstPlaylist.Items.Contains(file))
+                    {
+                        lstPlaylist.Items.Add(file);
+                    }
+                }
+
+                // 如果原本清單是空的，且現在有東西了，就自動播放剛加入的第一首
+                if (lstPlaylist.SelectedIndex == -1 && lstPlaylist.Items.Count > 0)
+                {
+                    lstPlaylist.SelectedIndex = 0;
+                    wmpVideo.URL = lstPlaylist.SelectedItem.ToString();
+                }
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.play(); // 播放
+        }
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.pause(); // 暫停
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.stop(); // 停止
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            wmpVideo.uiMode = "none";
+        }
+
+        private void lstPlaylist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstPlaylist_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstPlaylist.SelectedItem != null)
+            {
+                // 將播放器的 URL 設定為清單中選取的檔案路徑 [cite: 403]
+                wmpVideo.URL = lstPlaylist.SelectedItem.ToString();
+                wmpVideo.Ctlcontrols.play(); 
+    }
+        }
+
+        private void picPlay_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.play(); // 播放
+        }
+
+        private void picPause_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.pause(); // 暫停
+        }
+
+        private void picStop_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.stop(); // 停止
+        }
+    }
+}
